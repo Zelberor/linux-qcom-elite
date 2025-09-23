6e9139021f255d48e66f274ce7b8f578a17b5a77
Hangxiang Ma <hangxiang.ma@oss.qualcomm.com>
2025-09-22 21:51:40 -0700

    media: qcom: camss: vfe: Add support for VFE 1080
    Add more detailed resource information for VFE and VFE Lite devices
    along with a driver for the 1080 version of those that is responsible
    for bus write master configuration and buffer address update.
    
    The FULL front end modules in Kaanapali camera subsystem are called TFEs
    (Thin Front End), however, retaining the name VFE at places to maintain
    consistency and avoid unnecessary code changes.
    
    Co-developed-by: Atiya Kailany <atiya.kailany@oss.qualcomm.com>
    Signed-off-by: Atiya Kailany <atiya.kailany@oss.qualcomm.com>
    Signed-off-by: Hangxiang Ma <hangxiang.ma@oss.qualcomm.com>
    Signed-off-by: Jingyi Wang <jingyi.wang@oss.qualcomm.com>

--- drivers/media/platform/qcom/camss/camss-vfe.c
+++ drivers/media/platform/qcom/camss/camss-vfe.c
@@ -352,6 +352,7 @@ static u32 vfe_src_pad_code(struct vfe_line *line, u32 sink_code,
 	case CAMSS_8550:
 	case CAMSS_8650:
 	case CAMSS_8775P:
+	case CAMSS_KAANAPALI:
 	case CAMSS_X1E80100:
 		switch (sink_code) {
 		case MEDIA_BUS_FMT_YUYV8_1X16:
@@ -524,7 +525,8 @@ int vfe_enable_output_v2(struct vfe_line *line)
 
 	spin_lock_irqsave(&vfe->output_lock, flags);
 
-	ops->reg_update_clear(vfe, line->id);
+	if (ops->reg_update_clear)
+		ops->reg_update_clear(vfe, line->id);
 
 	if (output->state > VFE_OUTPUT_RESERVED) {
 		dev_err(vfe->camss->dev,
@@ -551,7 +553,10 @@ int vfe_enable_output_v2(struct vfe_line *line)
 		output->gen2.active_num++;
 		ops->vfe_wm_update(vfe, output->wm_idx[0],
 				   output->buf[i]->addr[0], line);
-		ops->reg_update(vfe, line->id);
+
+		/* Deferring the reg update until after CSID config */
+		if (vfe->camss->res->version != CAMSS_KAANAPALI)
+			ops->reg_update(vfe, line->id);
 	}
 
 	spin_unlock_irqrestore(&vfe->output_lock, flags);
@@ -2011,6 +2016,7 @@ static int vfe_bpl_align(struct vfe_device *vfe)
 	case CAMSS_8550:
 	case CAMSS_8650:
 	case CAMSS_8775P:
+	case CAMSS_KAANAPALI:
 	case CAMSS_X1E80100:
 		ret = 16;
 		break;
