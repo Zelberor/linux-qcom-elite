4a7788828c16f28d7145aa7eaeb5cd9be2b4375a
ZXlieC <xlie7669@gmail.com>
2025-11-23 16:36:00 +0200

    remoteproc: qcom: pas: Fixing region_assign_idx
    Qualcomm sm8750 and kaanapali SOCs have region_assign_idx 3 but in mainline it 2 which is incorrect. This mistake calls remoteproc crashing on this SOCs.
    
    Source: https://github.com/OnePlusOSS/android_kernel_oneplus_sm8850/blob/oneplus/sm8850_b_16.0.0_oneplus_15/drivers/remoteproc/qcom_q6v5_pas.c#L2672
    
    Signed-off-by: Kompanets Nazar <xlie7669@gmail.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -1583,7 +1583,7 @@ static const struct qcom_pas_data sm8750_mpss_resource = {
 	.sysmon_name = "modem",
 	.ssctl_id = 0x12,
 	.smem_host_id = 1,
-	.region_assign_idx = 2,
+	.region_assign_idx = 3,
 	.region_assign_count = 2,
 	.region_assign_vmid = QCOM_SCM_VMID_MSS_MSA,
 };
