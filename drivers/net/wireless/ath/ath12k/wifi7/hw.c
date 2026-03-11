a957b07de2f998e095b52b78bc631a82f5366a35
Gianni Spadoni <me@gio.blue>
2026-03-11 16:19:06 +0800

    wifi: ath12k: disable CNSS_QDSS_CFG_MISS_V01 for the WCN7860
    This chip requires this change currently to negate a firmware crash. Follows up on the QDSS support in the previous commit.
    
    Signed-off-by: Gianni Spadoni <me@gio.blue>

--- drivers/net/wireless/ath/ath12k/wifi7/hw.c
+++ drivers/net/wireless/ath/ath12k/wifi7/hw.c
@@ -768,8 +768,7 @@ static const struct ath12k_hw_params ath12k_wifi7_hw_params[] = {
 
 		.wmi_init = ath12k_wifi7_wmi_init_wcn7850,
 
-		.qmi_cnss_feature_bitmap = BIT(CNSS_QDSS_CFG_MISS_V01) |
-					   BIT(CNSS_PCIE_PERST_NO_PULL_V01) |
+		.qmi_cnss_feature_bitmap = BIT(CNSS_PCIE_PERST_NO_PULL_V01) |
 					   BIT(CNSS_AUX_UC_SUPPORT_V01),
 
 		.rfkill_pin = 0,
