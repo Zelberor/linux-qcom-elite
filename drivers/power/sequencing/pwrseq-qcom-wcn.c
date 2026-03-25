1bb9cba3cfa3600c2ac9ef70c6e0cb5a6c30232a
Teguh Sobirin <teguh@sobir.in>
2026-03-25 21:37:37 +0800

    power: sequencing: qcom-wcn: add support for WCN7860
    Signed-off-by: Teguh Sobirin <teguh@sobir.in>

--- drivers/power/sequencing/pwrseq-qcom-wcn.c
+++ drivers/power/sequencing/pwrseq-qcom-wcn.c
@@ -401,6 +401,22 @@ static const struct pwrseq_qcom_wcn_pdata pwrseq_wcn7850_of_data = {
 	.targets = pwrseq_qcom_wcn_targets,
 };
 
+static const char *const pwrseq_wcn7860_vregs[] = {
+	"vddio",
+	"vddio1p2",
+	"vddaon",
+	"vdddig",
+	"vddrfa1p2",
+	"vddrfa1p8",
+};
+
+static const struct pwrseq_qcom_wcn_pdata pwrseq_wcn7860_of_data = {
+	.vregs = pwrseq_wcn7860_vregs,
+	.num_vregs = ARRAY_SIZE(pwrseq_wcn7860_vregs),
+	.pwup_delay_ms = 50,
+	.targets = pwrseq_qcom_wcn_targets,
+};
+
 static int pwrseq_qcom_wcn_match_regulator(struct pwrseq_device *pwrseq,
 					   struct device *dev,
 					   const char *name)
@@ -575,6 +591,10 @@ static const struct of_device_id pwrseq_qcom_wcn_of_match[] = {
 	{
 		.compatible = "qcom,wcn7850-pmu",
 		.data = &pwrseq_wcn7850_of_data,
+	},
+	{
+		.compatible = "qcom,wcn7860-pmu",
+		.data = &pwrseq_wcn7860_of_data,
 	},
 	{
 		.compatible = "qcom,wcn6750-pmu",
